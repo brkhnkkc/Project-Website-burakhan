24d00051abd1e4dee1bed79bf772b032bfb6d923
brkhnkkc <116767550+brkhnkkc@users.noreply.github.com>
2026-04-24 14:17:15 -0400

    Fix import path for Navbar component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
-import { Navbar } from "../components/navBar";
+import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillSection } from "../components/SkillsSection";
